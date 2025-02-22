ea548a89284e6e88e7be1f8db5b4a672957646d4
mattp18 <mpuent18@gmail.com>
2025-02-21 21:02:13 -0500

    remove driver test file

--- src/main/java/Driver.java
+++
@@ -1,6 +0,0 @@
-public class Driver {
-    public static void main(String [] args) {
-        ClamAVScanner scanner = new ClamAVScanner("localhost", 3310);
-        scanner.scanFile("novirus.txt");
-    }
-} 
